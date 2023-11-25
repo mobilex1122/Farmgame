58999656de9c26232981e6c3a29858d50bd204d0
Jan Palma <60009551+mobilex1122@users.noreply.github.com>
2023-11-25 12:45:00 +0100

    Added test entity + Render System

---
+++ core/src/cz/janpalma/farmgame/components/PositionComponent.java
@@ -0,0 +1,9 @@
+package cz.janpalma.farmgame.components;
+
+
+import com.badlogic.ashley.core.Component;
+
+public class PositionComponent implements Component {
+    public float x = 0;
+    public float y = 0;
+}
